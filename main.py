fe740a9c34b0a3d0a9d292a7569865eab1a1c5d7
Willie Macharia <ngangawillie84@gmail.com>
2023-03-07 19:59:36 +0200

    Added streamlit

---
+++ main.py
@@ -0,0 +1,56 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import streamlit as st
+import pandas as pd
+import numpy as np
+import plotly.graph_objects as go
+import plotly.express as px
+from datetime import datetime
+import pdfminer
+import pandas as pd
+from io import StringIO
+import streamlit as st
+
+##LOAD DATA
+@st.cache
+def load_data():
+    df_agg = pd.read_csv('Aggregated_Metrics_By_Video.csv').iloc[1:, :]
+    df_agg_sub = pd.read_csv('Aggregated_Metrics_By_Country_And_Subscriber_Status.csv').iloc[1:, :]
+    df_agg.columns = ['Video', 'Video title', 'Video publish time', 'Comments added', 'Shares', 'Dislikes', 'Likes',
+                      'Subscribers lost', 'Subscribers gained', 'RPM(USD)', 'CPM(USD)', 'Average % viewed',
+                      'Average view duration',
+                      'Views', 'Watch time (hours)', 'Subscribers', 'Your estimated revenue (USD)', 'Impressions',
+                      'Impressions ctr(%)']
+    df_agg['Video publish time'] = pd.to_datetime(df_agg['Video publish time'])
+    df_agg['Average view duration'] = df_agg['Average view duration'].apply(lambda x: datetime.strptime(x, '%H:%M:%S'))
+    df_agg['Avg_duration_sec'] = df_agg['Average view duration'].apply(lambda x: x.second + x.minute * 60 + x.hour * 3600)
+    df_agg['Engagement_ratio'] = (df_agg['Comments added'] + df_agg['Shares'] + df_agg['Dislikes'] + df_agg[
+        'Likes']) / df_agg.Views
+    df_agg['Views / sub gained'] = df_agg['Views'] / df_agg['Subscribers gained']
+    df_agg.sort_values('Video publish time', ascending=False, inplace=True)
+    df_agg_sub = pd.read_csv('Aggregated_Metrics_By_Country_And_Subscriber_Status.csv')
+    df_comments = pd.read_csv('Aggregated_Metrics_By_Video.csv')
+    df_time = pd.read_csv('Video_Performance_Over_Time.csv')
+    df_time['Date'] = pd.to_datetime(df_time['Date'])
+    return df_agg, df_agg_sub, df_comments, df_time
+
+add_sidebar = st.sidebar.selectbox('Aggregate or Individual Video', ('Aggregate Metrics','Individual Video Analysis'))
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+    st.button("Click here for the process")
+    d=st.button("Abled")
+    st.write('Hello, *World!* :sunglasses:')
+
+    st.markdown('Streamlit is **_really_ cool**.')
+    st.markdown("This text is: red[colored red], and this is **:blue[colored] ** and bold.")
+    st.markdown(":green[$\sqrt{x^2+y^2}=1$] is a Pythagorean identity. :pencil:")
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
